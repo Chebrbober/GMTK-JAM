8248ba2d3a3a3bf37fdfec119602194a1cb8b4b4
beaver <meyar2223@gmail.com>
2026-07-23 09:31:37 +0300

    add food script

---
+++ scripts/food.gd
@@ -0,0 +1,18 @@
+class_name Food extends Node2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@export var cutsRequired: int = 5
+@export var cutsDone: int = 0
+@export var available_sprites: Array[Texture2D]
+
+signal finished
+
+func _ready() -> void:
+	if available_sprites != null:
+		sprite_2d.texture = available_sprites[randi_range(0, available_sprites.size() - 1)]
+	
+func cut() -> void:
+	cutsDone += 1
+	
+	if cutsDone == cutsRequired:
+		emit_signal("finished")
