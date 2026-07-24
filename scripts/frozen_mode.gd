e3965173ea6b09555ab392848f9e39843a0c9206
beaver <meyar2223@gmail.com>
2026-07-24 15:10:44 +0300

    Add shaders to frozen mode, and smooth it. Fix issues with pausing

--- scripts/frozen_mode.gd
+++ scripts/frozen_mode.gd
@@ -4,7 +4,10 @@ extends Node
 @onready var manual_control: Control = $"../../CutBar/PanelContainer/MarginContainer/ManualControl"
 @onready var panel_container: PanelContainer = $"../../CutBar/PanelContainer"
 @onready var yellow_zone: ColorRect = $"../../CutBar/PanelContainer/MarginContainer/ManualControl/YellowZone"
+@onready var shader_rect: ColorRect = $ShaderRect
+@onready var material := shader_rect.material
 @export var frozen_zone: ColorRect
+var tween: Tween
 
 func create_zone() -> void:
 	print('creating zone')
@@ -23,16 +26,44 @@ func create_zone() -> void:
 	manual_control.add_child(frozen_zone)
 
 func enable_mode() -> void:
-	print("enabled zone")
-	Engine.time_scale = 0.5
+	if tween:
+		tween.kill()
+		
 	cut_bar.temp_speed = cut_bar.speed*1.25
+		
+	print("enabled zone")
+	tween = create_tween()
+	tween.tween_property(Engine, "time_scale", 0.5, 2).from_current()
+	
+	shader_rect.visible = true
+	tween.parallel().tween_method(func(value):
+		material.set_shader_parameter("strength", value), 0.0, 0.8, 2.0)
+	tween.parallel().tween_method(func(value):
+		material.set_shader_parameter("border_size", value), 0.0, 0.1, 2.0)
+	tween.parallel().tween_method(func(value):
+		material.set_shader_parameter("distortion", value), 0.0, 0.008, 2.0)
+	
 	await get_tree().create_timer(5.0).timeout
 	disable_mode()
 
 func disable_mode() -> void:
 	print("frozen zone disabled")
-	Engine.time_scale = 1
 	cut_bar.temp_speed = -1
+	
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	
+	tween.tween_property(Engine, "time_scale", 1, 2).from_current()
+	
+	tween.parallel().tween_method(func(value):
+		material.set_shader_parameter("strength", value), 0.8, 0.0, 2.0)
+	tween.parallel().tween_method(func(value):
+		material.set_shader_parameter("border_size", value), 0.1, 0.0, 2.0)
+	tween.parallel().tween_method(func(value):
+		material.set_shader_parameter("distortion", value), 0.008, 0.0, 2.0)
+	await tween.finished
+	shader_rect.visible = false
 
 func _on_cut_bar_mouse_clicked(type: CutType.Result) -> void:
 	if type == CutType.Result.FROZEN:
--- scripts/transition_scene.gd
+++ scripts/transition_scene.gd
@@ -6,14 +6,18 @@ func _ready() -> void:
 	visible = false
 
 func transition_to(path_to_scene: String) -> void:
+	if Engine.time_scale != 1:
+		Engine.time_scale = 1
 	animation_player.play("trans_animation")
 	var timer = get_tree().create_timer(1.0)
 	await timer.timeout
+	get_tree().paused = true
 	get_parent().process_mode = Node.PROCESS_MODE_DISABLED
 	switch_scene(path_to_scene)
 	await animation_player.animation_finished
 	visible = false
 	get_parent().process_mode = Node.PROCESS_MODE_ALWAYS
+	get_tree().paused = false
 	
 func switch_scene(destination: String) -> void:
 	get_tree().change_scene_to_file(destination)
