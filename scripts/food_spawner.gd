651670c6cb7eb03c19a9c55631b16e3588c27978
beaver <meyar2223@gmail.com>
2026-07-26 17:57:52 +0300

    fix some issues with directory finding and empty array

--- scripts/food_spawner.gd
+++ scripts/food_spawner.gd
@@ -1,7 +1,6 @@
 class_name FoodSpawner extends Node
 
 @export var food_scene: PackedScene
-@export_dir var resources_folder
 @export var foods: Array[FoodResource]
 @onready var wind_audio: AudioStreamPlayer = $WindAudio
 @onready var screen_size = get_viewport().get_visible_rect().size
@@ -15,34 +14,7 @@ var cut_acces: bool = false
 signal cut_acces_changed(can_cut: bool)
 
 func _ready() -> void:
-	await search_for_food()
 	spawn()
-	
-func search_for_food() -> void:
-	foods.clear()
-
-	var dir := DirAccess.open(resources_folder)
-	if dir == null:
-		push_error("Couldn't open folder: " + resources_folder)
-		return
-
-	dir.list_dir_begin()
-
-	while true:
-		var file := dir.get_next()
-
-		if file == "":
-			break
-		if dir.current_is_dir():
-			continue
-		if file.ends_with(".tres"):
-			var resource := load(resources_folder.path_join(file))
-			if resource is FoodResource:
-				foods.append(resource)
-
-	dir.list_dir_end()
-
-	print("Loaded ", foods.size(), " food resources.")
 	
 func spawn() -> void:
 	var food = food_scene.instantiate()
